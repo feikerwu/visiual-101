5dacc3af2ad504804cb4b2da54f35f3e833411de
feikerwu <feikerwu@gmail.com>
2023-04-11 18:13:45 +0800

    feat: calendar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,13 @@
 import '../styles/globals.css';
 import '../styles/custom.css';
+import * as d3 from 'd3';
 
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  if (window) {
+    window.d3 = d3;
+  }
   return <Component {...pageProps} />;
 }
 
